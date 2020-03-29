ea3d0cb559fd6031e8dec689ab77b7d485a9224b
IzaiAnda <44356898+IzaiAnda@users.noreply.github.com>
2020-03-29 20:37:58 +0200

    Mas Clases

--- stock_manager/src/main/java/es/deusto/spq/ConexionBD.java
+++ stock_manager/src/main/java/es/deusto/spq/ConexionBD.java
@@ -10,32 +10,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import java.util.logging.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexionBD {
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.SQLException;
-
-	public class DBconnection {
-		private static String dbhost = "jdbc:mysql://localhost:3306/stock_managerDB";
-		private static String username = "root";
-		private static String password = "1234Clave";
-		private static Connection conn;
+	
+		private static final String dbhost = "jdbc:mysql://localhost:3306/stock_ managerDB";
+		private static final String username = "root";
+		private static final String password = "1234Clave";
+		private Connection conn;
 		
 		@SuppressWarnings("finally")
-		public static Connection createNewDBconnection() {
+		public Connection createNewDBconnection() {
 			try  {	
 				conn = DriverManager.getConnection(
 						dbhost, username, password);	
-			} catch (SQLException e) {
+			} catch (final SQLException e) {
 				System.out.println("Cannot create database connection");
 				e.printStackTrace();
 			} finally {
 				return conn;	
 			}		
-		}
-	}
-	
+		}	
 
 }
--- stock_manager/src/main/java/es/deusto/spq/clases/Cesta.java
+++ stock_manager/src/main/java/es/deusto/spq/clases/Cesta.java
@@ -1,9 +1,34 @@
 package es.deusto.spq.clases;
 
+import java.util.HashMap;
+
 public class Cesta {
 
     private int ID;
+    private HashMap<Articulo,Integer> articulos;
     private String estado;
-    private 
+
+    public Cesta(){
+        this.articulos = new HashMap<>();
+    }
+
+    public void addArticulo(Articulo articulo, int cantidad){
+        this.articulos.put(articulo, cantidad);
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public int getID(){
+        return this.ID;
+    }
+    public void setID(int ID){
+        this.ID = ID;
+    }
 
 }
