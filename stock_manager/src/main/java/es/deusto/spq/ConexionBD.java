08938b63b7a287080bc77a48b2dfa45681d27aa3
mikelmartinez999 <mikelmartinez999@opendeusto.es>
2020-03-29 20:19:39 +0200

    Conexion realizada

--- stock_manager/src/main/java/es/deusto/spq/ConexionBD.java
+++ stock_manager/src/main/java/es/deusto/spq/ConexionBD.java
@@ -10,32 +10,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import java.util.logging.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class ConexionBD {
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.SQLException;
+private String dbhost = "jdbc:mysql://localhost/stock_managerDB";
+private String username = "root";
+private String password = "1234Clave";
+private static String bd = "stock_managerDB";
+private Connection conn = null;
 
-	public class DBconnection {
-		private static String dbhost = "jdbc:mysql://localhost:3306/stock_managerDB";
-		private static String username = "root";
-		private static String password = "1234Clave";
-		private static Connection conn;
-		
-		@SuppressWarnings("finally")
-		public static Connection createNewDBconnection() {
-			try  {	
-				conn = DriverManager.getConnection(
-						dbhost, username, password);	
-			} catch (SQLException e) {
-				System.out.println("Cannot create database connection");
-				e.printStackTrace();
-			} finally {
-				return conn;	
-			}		
+@SuppressWarnings("finally")
+public Connection createNewDBconnection() {
+	try  {	
+		Class.forName("com.mysql.jdbc.Connection");
+		conn = (Connection)DriverManager.getConnection(dbhost, username, password);	
+		if(conn != null) {
+			System.out.println("Conexion a base de datos " + dbhost +"....OK");
 		}
-	}
+	} catch (SQLException e) {
+		System.out.println("Cannot create database connection");
+		e.printStackTrace();
+	} finally {
+		return conn;	
+	}		
+}
+
 	
 
 }
