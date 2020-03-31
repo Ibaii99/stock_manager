facd3e2763a8d3078414e1bb91306fd8ab9fe96c
jokin <jokinme99@opendeusto.es>
2020-03-31 14:40:41 +0200

    arreglado

--- stock_manager/src/test/java/es/deusto/spq/data/Opinion_test.java
+++ stock_manager/src/test/java/es/deusto/spq/data/Opinion_test.java
@@ -26,22 +26,22 @@ public class Opinion_test {
 	}
 	@Test
 	public void id_clienteTest() {
-		assertEquals(cliente.getID(), 1);
+		assertEquals(cliente.getID_cliente(), 1);
 	}
 	@Test
 	public void nombre_clienteTest() {
-		assertEquals(cliente.getNombre(), "jon");
+		assertEquals(cliente.getNombre_cliente(), "jon");
 	}
 	@Test
 	public void email_clienteTest() {
-		assertEquals(cliente.getEmail(), "jon@gmail.com");
+		assertEquals(cliente.getEmail_cliente(), "jon@gmail.com");
 	}
 	@Test 
 	public void contrasenya_cliente() {
-		assertEquals(cliente.getContrasenya(), "Deusto1");
+		assertEquals(cliente.getContrasenya_cliente(), "Deusto1");
 	}
 	@Test
 	public void direccion_cliente() {
-		assertEquals(cliente.getDireccion(), "Deusto kalea");
+		assertEquals(cliente.getDireccion_cliente(), "Deusto kalea");
 	}
 }
