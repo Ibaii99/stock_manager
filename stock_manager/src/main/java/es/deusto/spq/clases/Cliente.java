45958037f7a62229add7d761d8b9db3d7ffd9d15
mikelmartinez999 <mikelmartinez999@opendeusto.es>
2020-03-31 12:13:17 +0200

    Clase ClienteTest sin temrinar

--- stock_manager/src/main/java/es/deusto/spq/clases/Cliente.java
+++ stock_manager/src/main/java/es/deusto/spq/clases/Cliente.java
@@ -2,7 +2,9 @@ package es.deusto.spq.clases;
 
 public class Cliente {
     private int ID_cliente;
-    private String nombre_cliente;
+    
+
+	private String nombre_cliente;
     private String email_cliente;
     private String contrasenya_cliente;
     private String direccion_cliente;
@@ -14,6 +16,9 @@ public class Cliente {
         this.contrasenya_cliente = _nombre;
         this.direccion_cliente = _direccion;
     }
+	public int getID_cliente() {
+		return ID_cliente;
+	}
 
     public String getEmail_cliente() {
         return email_cliente;
@@ -30,78 +35,28 @@ public class Cliente {
     public void setNombre_cliente(String nombre_cliente) {
         this.nombre_cliente = nombre_cliente;
     }
+ 
+	public void setID_cliente(int iD_cliente) {
+		ID_cliente = iD_cliente;
+	}
 
-    /**
-     * @param ID the contrasenya to set
-     */
-    public void setID(int ID) {
-        this.ID_cliente = ID;
-    }
+	public String getContrasenya_cliente() {
+		return contrasenya_cliente;
+	}
 
-    /**
-     * @param contrasenya the contrasenya to set
-     */
-    public void setContrasenya(String contrasenya) {
-        this.contrasenya_cliente = contrasenya;
-    }
+	public void setContrasenya_cliente(String contrasenya_cliente) {
+		this.contrasenya_cliente = contrasenya_cliente;
+	}
 
-    /**
-     * @param direccion the direccion to set
-     */
-    public void setDireccion(String direccion) {
-        this.direccion_cliente = direccion;
-    }
+	public String getDireccion_cliente() {
+		return direccion_cliente;
+	}
 
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.contrasenya_cliente = email;
-    }
+	public void setDireccion_cliente(String direccion_cliente) {
+		this.direccion_cliente = direccion_cliente;
+	}
 
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.contrasenya_cliente = nombre;
-    }
-
-    /**
-     * @return the ID
-     */
-    public int getID() {
-        return ID_cliente;
-    }
-
-    /**
-     * @return the contrasenya
-     */
-    public String getContrasenya() {
-        return contrasenya_cliente;
-    }
-
-    /**
-     * @return the direccion
-     */
-    public String getDireccion() {
-        return direccion_cliente;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return contrasenya_cliente;
-    }
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return contrasenya_cliente;
-    }
-    
-    public void enviarMensajeServicioTecnico(int ID, Vendedor vendedor, String mensaje) {
+	public void enviarMensajeServicioTecnico(int ID, Vendedor vendedor, String mensaje) {
     	
 	}
 
---
+++ stock_manager/src/test/java/es/deusto/spq/clases/ClienteTest.java
@@ -0,0 +1,120 @@
+package es.deusto.spq.clases;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+
+import es.deusto.spq.clases.Cliente;
+
+import org.junit.Test;
+
+public class ClienteTest {
+	
+	private static Cliente c;
+
+	@BeforeClass
+	public static void initializationError() {
+		c = new Cliente(001, "Mikel", "mikelmartinez999@opendeusto.es", "12345", "Universidad de Deusto");
+		
+	}
+	
+	@Test
+	public void testGetEmail_cliente() {
+        String email = c.getEmail_cliente();
+        String esperado = "mikelmartinez999@opendeusto.es";
+        assertEquals("getEmail", email, esperado);
+    }
+	
+	@Test
+    public void testSetEmail_cliente(String email_cliente) {
+        c.setEmail_cliente(email_cliente);
+        String email_cambiado = c.getEmail_cliente();
+        assertEquals("setEmail", email_cliente, email_cambiado);
+    }
+
+	@Test
+    public void testGetNombre_cliente() {
+		String nombre = c.getNombre_cliente();
+		String esperado = "Mikel";
+        assertEquals("getNombre", nombre, esperado);
+    }
+
+	@Test
+	  public void testSetNombre_cliente(String nombre_cliente){
+		c.setNombre_cliente(nombre_cliente);
+		String nombre = c.getNombre_cliente();
+		assertEquals("SetNombre", nombre_cliente,nombre);
+  }
+
+    /**
+     * @param ID the contrasenya to set
+     */
+	@Test
+    public void testSetID_cliente(int ID) {
+        c.setID_cliente(ID);
+        int ID_new = c.getID_cliente();
+		assertEquals("SetNombre", ID, ID_new);
+    }
+
+    /**
+     * @param contrasenya the contrasenya to set
+     */
+	@Test
+    public void testSetContrasenya_cliente(String contrasenya) {
+        c.setContrasenya_cliente(contrasenya);
+        String contrasenya_new = c.getContrasenya_cliente();
+		assertEquals("SetContrasenya", contrasenya, contrasenya_new);
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+	@Test
+    public void testSetDireccion_cliente(String direccion) {
+       c.setDireccion_cliente(direccion);
+       String dir = c.getDireccion_cliente();
+		assertEquals("SetDireccion", direccion, dir);
+    }
+
+   
+    /**
+     * @return the ID
+     */
+	@Test
+    public void testGetID() {
+		int ID_cliente = c.getID_cliente();
+		int esperado = 001;
+        assertEquals("getNombre", ID_cliente, esperado);
+        
+    }
+
+    /**
+     * @return the contrasenya
+     */
+	@Test
+    public void testGetContrasenya_cliente() {
+        String contraseya = c.getContrasenya_cliente();
+        String esperado = "12345";
+        assertEquals("getContrasenya", contraseya, esperado);
+    }
+
+    /**
+     * @return the direccion
+     */
+	@Test
+    public void testGetDireccion_cliente() {
+        String dir = c.getDireccion_cliente();
+        String esperado = "Universidad de Deusto";
+        assertEquals("getDireccion", dir, esperado);
+    }
+
+   
+
+ 
+    
+	@Test
+    public void testEnviarMensajeServicioTecnico(int ID, Vendedor vendedor, String mensaje) {
+    	fail("Sin implementar por el momento");
+	}
+
+}
