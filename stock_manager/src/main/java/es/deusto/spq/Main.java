24cedbff573447497b4cd10474b086302e3b2ee0
Ibaii99 <ibai.guillen@opendeusto.es>
2020-04-01 03:25:54 +0200

    Execution fail fixed

--- stock_manager/src/main/java/es/deusto/spq/Main.java
+++ stock_manager/src/main/java/es/deusto/spq/Main.java
@@ -1,11 +1,13 @@
-package es.deusto.spq.main;
+package es.deusto.spq;
+
+import java.io.IOException;
+import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
-import java.net.URI;
+
 
 /**
  * Main class.
