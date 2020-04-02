64985720012b2c091a8930307dda6836bf84065f
Ibaii99 <ibai.guillen@opendeusto.es>
2020-04-02 21:18:06 +0200

    Dao class problem fixed

--- stock_manager/src/main/java/es/deusto/spq/data_access/DAO.java
+++ stock_manager/src/main/java/es/deusto/spq/data_access/DAO.java
@@ -28,12 +28,10 @@ import es.deusto.spq.data.Cliente;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class DAO {
-	//JDO
-	
-	private final PersistenceManagerFactory pmf;
-	private final PersistenceManager pm;
-	
-	
+	// JDO
+
+	private PersistenceManagerFactory pmf;
+	private PersistenceManager pm;
 
 	public DAO() {
 		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
@@ -56,81 +54,8 @@ public class DAO {
 
 		}
 	}
-	
-	
-	//Hacer stores de cada clase
-	//Hacer 
-	
-	//DATOS_DAO
-	private Cliente a = new Cliente("pepe", "pepe@gmail.com", "1234", "Barakaldo");
-	private Cliente b = new Cliente("luis", "luis@gmail.com", "1234", "Universidad Deusto");
-	private Cliente c = new Cliente("mani", "mani@gmail.com", "1234", "Bilbao");
-	private Cliente d = new Cliente("sandra", "sandra@gmail.com", "1234", "Universidad");
-	private Cliente e = new Cliente("Ane", "ane@gmail.com", "1234", "DeustoTech");
-	private Vendedor f = new Vendedor("MikelVendedor", "mikelVendedor@gmail.com");
-	private Vendedor g = new Vendedor("JokinVendedor", "jokinVendedor@gmail.com");
-	private Vendedor h = new Vendedor("IbaiVendedor", "ibailVendedor@gmail.com");
-	private Vendedor i = new Vendedor("IzaiVendedor", "izaiVendedor@gmail.com");
-	private Vendedor j = new Vendedor("UnaiVendedor", "unaiVendedor@gmail.com");
-	private Articulo manzana = new Articulo("manzana", new Date(04/03/120), 1.20f, 400, "rica manzana", 0.95f, Categoria.FRUTA);
-	private Articulo lechuga = new Articulo("lechuga", new Date(04/03/120), 1.20f, 400, "rica lechuga", 0.95f, Categoria.VEGETAL);
-	private Articulo fresa = new Articulo("pimiento", new Date(04/03/120), 1.20f, 400, "rica fresa", 0.95f, Categoria.VEGETAL);
-	private Articulo calabaza = new Articulo("lechuga", new Date(04/03/120), 1.20f, 400, "rica calabza", 0.95f, Categoria.VEGETAL);
-	private Articulo pimiento = new Articulo("lechuga", new Date(04/03/120), 1.20f, 400, "rico pimiento", 0.95f, Categoria.VEGETAL);
-	private Articulo a1 = new Articulo("coliflor",new Date(04/03/120), 1.20f, 400, "rica coliflor", 1.05f, Categoria.FRUTA);
-	private Articulo a2 = new Articulo("pan",new Date(04/03/120), 1.20f, 400, "rica pan", 1.05f, Categoria.FRUTA);
-	private Articulo a3 = new Articulo("agua",new Date(04/03/120), 1.20f, 400, "rica agua", 1.05f, Categoria.BEBIDA);
-	private Articulo a4 = new Articulo("Mandarina",new Date(04/03/120), 1.20f, 400, "rica mandarina", 1.05f, Categoria.CARNICERIA);
-	private ArrayList<Cesta> listaCestas= new ArrayList<Cesta>();
-	private Cliente c1 = new Cliente("jokin", "jokin@gmail.com", "hola", "Deusto kalea 1");
-	private Cliente c2 = new Cliente("aitor", "aitor@gmail.com", "hola", "Deusto kalea 1");
-	private Integer cantidad1 = 400;
-	private Integer cantidad2 = 500;
-	private Integer cantidad3 = 300;
-	private Integer cantidad4 = 600;
-	private Cliente mikel = new Cliente("mikel", "mikel@gmail.com", "1234", "Barakaldo");
-	private	Cliente jokin = new Cliente("jokin", "jokin@gmail.com", "1234", "Universidad Deusto");
-	private	Cliente ibai = new Cliente("ibai", "ibail@gmail.com", "1234", "Bilbao");
-	private	Cliente izai = new Cliente("izaia", "izai@gmail.com", "1234", "Universidad");
-	private	Cliente unai = new Cliente("Unai", "unai@gmail.com", "1234", "DeustoTech");
-	private	Opinion k = new Opinion("Me ha encantado la lechuga", 7,mikel);
-	private	Opinion l = new Opinion("No me ha gusatdo el pimiento",1,jokin);
-	private	Opinion m = new Opinion("No estaba buena del todo la calabaza pero estaba fresca", 6, ibai);
-	private	Opinion n = new Opinion("No estaban fresca del todo las fresas pero estaban buenas", 5, izai);
-	private	Opinion o = new Opinion("Habeis hecho un excelente trabajo",10,unai);
-	DAO dao = new DAO();
 
-//ARTÍCULOS
-	//STORE de artículos
-	 @POST
-		@Path("postArticulos")
-	    @Consumes(MediaType.APPLICATION_JSON)
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public ArrayList<Articulo> listaArticulos() {
-			DAO dao = new DAO();
-			ArrayList<Articulo> listaArticulos= new ArrayList<Articulo>();
-			listaArticulos.add(a1);
-			listaArticulos.add(a2);
-			listaArticulos.add(a3);
-			listaArticulos.add(a4);
-			listaArticulos.add(lechuga);
-			listaArticulos.add(pimiento);
-			listaArticulos.add(fresa);
-			listaArticulos.add(calabaza);
-			listaArticulos.add(manzana);
-			dao.store(a1);
-			dao.store(a2);
-			dao.store(a3);
-			dao.store(a4);
-			dao.store(lechuga);
-			dao.store(pimiento);
-			dao.store(fresa);
-			dao.store(calabaza);
-			dao.store(manzana);
-			return listaArticulos;
-		}
-	
-	//GET de lista de artículos
+	// GET de lista de artículos
 	public List<Articulo> getArticulos() {
 		final List<Articulo> ret = new ArrayList<Articulo>();
 		final Transaction tx = pm.currentTransaction();
@@ -151,63 +76,24 @@ public class DAO {
 		}
 		return ret;
 	}
-	//GET de un artículo
-		public Articulo getArticulo(long idArticulo) {
-			Articulo a = null;
-			ArrayList<Articulo> articulos = new ArrayList<Articulo>();
-			articulos = (ArrayList<Articulo>) this.getArticulos();
-			for(int i = 0; i<articulos.size(); i++) {
-				a = articulos.get(i);
-				if(idArticulo == a.getId()) {
-					return a;
-				}
-			}
-			return a;
-		}
 
-	 
-//GET de artículos	 
-//		@GET
-//	    @Path("get2")
-//	    @Produces(MediaType.APPLICATION_JSON)
-//	    public String devuelveArticulos() {
-//	        return "Articulo devuelto!";
-//	    }
-	
-		
-//CLIENTES
-		//STORE de clientes
-	 @POST
-		@Path("postClientes")
-	    @Consumes(MediaType.APPLICATION_JSON)
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public ArrayList<Cliente> listaClientes() {
-			ArrayList<Cliente> listaClientes= new ArrayList<Cliente>();
-			listaClientes.add(a);
-			listaClientes.add(b);
-			listaClientes.add(c);
-			listaClientes.add(d);
-			listaClientes.add(e);
-			listaClientes.add(mikel);
-			listaClientes.add(jokin);
-			listaClientes.add(ibai);
-			listaClientes.add(izai);
-			listaClientes.add(unai);
-			dao.store(a);
-			dao.store(b);
-			dao.store(c);
-			dao.store(d);
-			dao.store(e);
-			dao.store(mikel);
-			dao.store(jokin);
-			dao.store(ibai);
-			dao.store(izai);
-			dao.store(unai);
-			return listaClientes;
+	// GET de un artículo
+	public Articulo getArticulo(long idArticulo) {
+		Articulo a = null;
+		ArrayList<Articulo> articulos = new ArrayList<Articulo>();
+		articulos = (ArrayList<Articulo>) this.getArticulos();
+		for (int i = 0; i < articulos.size(); i++) {
+			a = articulos.get(i);
+			if (idArticulo == a.getId()) {
+				return a;
+			}
 		}
-	//GET de lista de clientes
-	public List<Cliente> getClientes(){
-		List <Cliente> ret = new ArrayList<Cliente>();
+		return a;
+	}
+
+	// GET de lista de clientes
+	public List<Cliente> getClientes() {
+		List<Cliente> ret = new ArrayList<Cliente>();
 		Transaction tx = pm.currentTransaction();
 		try {
 			System.out.println("   * Retrieving an Extent for Cliente.");
@@ -226,59 +112,32 @@ public class DAO {
 		}
 		return ret;
 	}
-	//GET de un cliente
+
+	// GET de un cliente
 	public Cliente getCliente(String email, String contrasenya) {
 		Cliente c = null;
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 		clientes = (ArrayList<Cliente>) this.getClientes();
-		for(int i = 0; i<clientes.size(); i++) {
+		for (int i = 0; i < clientes.size(); i++) {
 			c = clientes.get(i);
-			if(email.equals(c.getEmail_cliente()) && email.equals(c.getContrasenya_cliente())) {
+			if (email.equals(c.getEmail_cliente()) && email.equals(c.getContrasenya_cliente())) {
 				return c;
 			}
 		}
 		return c;
 	}
-	
-//GET de clientes
-//		@GET
-//	    @Path("get0")
-//	    @Produces(MediaType.APPLICATION_JSON)
-//	    public String devuelveClientes() {
-//	        return "Cliente devuelto!";
-//		}
 
-//CESTAS
-	//STORE de cestas
-	@POST
-	@Path("postCestas")
-	@Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Cesta> listaCestas() {
-		DAO dao = new DAO();
-		
-		List<Articulo>listaArticulos1 = new ArrayList<Articulo>();
-		List<Articulo>listaArticulos2 = new ArrayList<Articulo>();
-		List<Integer> listaCantidades1 = new ArrayList<Integer>();
-		List<Integer> listaCantidades2 = new ArrayList<Integer>();
-		listaArticulos1.add(a1);
-		listaArticulos1.add(a2);
-		listaArticulos2.add(a3);
-		listaArticulos2.add(a4);
-		listaCantidades1.add(cantidad1);
-		listaCantidades1.add(cantidad2);
-		listaCantidades2.add(cantidad3);
-		listaCantidades2.add(cantidad4);
-		Cesta a = new Cesta(1, c1, listaArticulos1, listaCantidades1, Estado.ACTUAL);
-		Cesta b = new Cesta(2, c2, listaArticulos2, listaCantidades2, Estado.ACTUAL);
-		dao.store(a);
-		dao.store(b);
-		return listaCestas;
-	}
-	
-    //GET de lista de cestas
-	public List<Cesta> getCestas(){
-		List <Cesta> ret = new ArrayList<Cesta>();
+	// GET de clientes
+	// @GET
+	// @Path("get0")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public String devuelveClientes() {
+	// return "Cliente devuelto!";
+	// }
+
+	// GET de lista de cestas
+	public List<Cesta> getCestas() {
+		List<Cesta> ret = new ArrayList<Cesta>();
 		Transaction tx = pm.currentTransaction();
 		try {
 			System.out.println("   * Retrieving an Extent for Cestas.");
@@ -297,51 +156,23 @@ public class DAO {
 		}
 		return ret;
 	}
-	
+
 	public Cesta getCesta(long idCesta) {
 		Cesta c = null;
 		ArrayList<Cesta> cestas = new ArrayList<Cesta>();
 		cestas = (ArrayList<Cesta>) this.getCestas();
-		for(int i = 0; i<cestas.size(); i++) {
+		for (int i = 0; i < cestas.size(); i++) {
 			c = cestas.get(i);
-			if(idCesta == c.getId()) {
+			if (idCesta == c.getId()) {
 				return c;
 			}
 		}
 		return c;
 	}
-//GET de cestas
-//	@GET
-//    @Path("get3")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String devuelveCestas() {
-//        return "Cesta devuelta!";
-//	}
-    
-//OPINION
-	//STORE de opiniones
-    @POST
-	@Path("postOpiniones")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Opinion> listaOpiniones() {
-        ArrayList<Opinion> listaOpiniones= new ArrayList<Opinion>();
-       
-		listaOpiniones.add(k);
-		listaOpiniones.add(l);
-		listaOpiniones.add(m);
-		listaOpiniones.add(n);
-		listaOpiniones.add(o);
-		dao.store(k);
-		dao.store(l);
-		dao.store(m);
-		dao.store(n);
-		dao.store(o);
-		return listaOpiniones;
-	}
-	//GET de lista de opiniones
-    public List<Opinion> getOpiniones(){
-		List <Opinion> ret = new ArrayList<Opinion>();
+
+	// GET de lista de opiniones
+	public List<Opinion> getOpiniones() {
+		List<Opinion> ret = new ArrayList<Opinion>();
 		Transaction tx = pm.currentTransaction();
 		try {
 			System.out.println("   * Retrieving an Extent for Opiniones.");
@@ -360,51 +191,24 @@ public class DAO {
 		}
 		return ret;
 	}
-    //GET de una unica opinion
+
+	// GET de una unica opinion
 	public Opinion getOpinion(long id) {
 		Opinion c = null;
 		ArrayList<Opinion> Opiniones = new ArrayList<Opinion>();
 		Opiniones = (ArrayList<Opinion>) this.getOpiniones();
-		for(int i = 0; i<Opiniones.size(); i++) {
+		for (int i = 0; i < Opiniones.size(); i++) {
 			c = Opiniones.get(i);
-			if(id == c.getId()) {
+			if (id == c.getId()) {
 				return c;
 			}
 		}
 		return c;
 	}
-	
-//GET de opinion
-//@GET
-//@Path("get4")
-//@Produces(MediaType.APPLICATION_JSON)
-//public String devuelveOpiniones() {
-//    return "Opiniones devuelto!";
-//}
 
-//VENDEDOR
-	//STORE de vendedores
-	@POST
-	@Path("postVendedores")
-	@Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Vendedor> listaVendedores() {
-		ArrayList<Vendedor> listaVendedores= new ArrayList<Vendedor>();
-		listaVendedores.add(f);
-		listaVendedores.add(g);
-		listaVendedores.add(h);
-		listaVendedores.add(i);
-		listaVendedores.add(j);
-		dao.store(f);
-		dao.store(g);
-		dao.store(h);
-		dao.store(i);
-		dao.store(j);
-		return listaVendedores;
-	}
-	//GET de lista de vendedores
-    public List<Vendedor> getVendedores(){
-		List <Vendedor> ret = new ArrayList<Vendedor>();
+	// GET de lista de vendedores
+	public List<Vendedor> getVendedores() {
+		List<Vendedor> ret = new ArrayList<Vendedor>();
 		Transaction tx = pm.currentTransaction();
 		try {
 			System.out.println("   * Retrieving an Extent for Vendedores.");
@@ -423,31 +227,161 @@ public class DAO {
 		}
 		return ret;
 	}
-    //GET de un unico vendedor
+
+	// GET de un unico vendedor
 	public Vendedor getVendedor(long id) {
 		Vendedor c = null;
 		ArrayList<Vendedor> Vendedores = new ArrayList<Vendedor>();
 		Vendedores = (ArrayList<Vendedor>) this.getVendedores();
-		for(int i = 0; i<Vendedores.size(); i++) {
+		for (int i = 0; i < Vendedores.size(); i++) {
 			c = Vendedores.get(i);
-			if(id == c.getId()) {
+			if (id == c.getId()) {
 				return c;
 			}
 		}
 		return c;
 	}
-//GET de vendedores
-//@GET
-//@Path("get1")
-//@Produces(MediaType.APPLICATION_JSON)
-//public String devuelveVendedores() {
-//    return "Vendedores devuelto!";
-//}
+	// GET de vendedores
+	// @GET
+	// @Path("get1")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public String devuelveVendedores() {
+	// return "Vendedores devuelto!";
+	// }
+	public void meter_datos() {
 
-	
-	//No cerrar la conexion hasta cerrar el programa
+		Cliente a = new Cliente("pepe", "pepe@gmail.com", "1234", "Barakaldo");
+		Cliente b = new Cliente("luis", "luis@gmail.com", "1234", "Universidad Deusto");
+		Cliente c = new Cliente("mani", "mani@gmail.com", "1234", "Bilbao");
+		Cliente d = new Cliente("sandra", "sandra@gmail.com", "1234", "Universidad");
+		Cliente e = new Cliente("Ane", "ane@gmail.com", "1234", "DeustoTech");
+		Vendedor f = new Vendedor("MikelVendedor", "mikelVendedor@gmail.com");
+		Vendedor g = new Vendedor("JokinVendedor", "jokinVendedor@gmail.com");
+		Vendedor h = new Vendedor("IbaiVendedor", "ibailVendedor@gmail.com");
+		Vendedor i = new Vendedor("IzaiVendedor", "izaiVendedor@gmail.com");
+		Vendedor j = new Vendedor("UnaiVendedor", "unaiVendedor@gmail.com");
+		Articulo manzana = new Articulo("manzana", new Date(04 / 03 / 120), 1.20f, 400, "rica manzana", 0.95f,
+				Categoria.FRUTA);
+		Articulo lechuga = new Articulo("lechuga", new Date(04 / 03 / 120), 1.20f, 400, "rica lechuga", 0.95f,
+				Categoria.VEGETAL);
+		Articulo fresa = new Articulo("pimiento", new Date(04 / 03 / 120), 1.20f, 400, "rica fresa", 0.95f,
+				Categoria.VEGETAL);
+		Articulo calabaza = new Articulo("lechuga", new Date(04 / 03 / 120), 1.20f, 400, "rica calabza", 0.95f,
+				Categoria.VEGETAL);
+		Articulo pimiento = new Articulo("lechuga", new Date(04 / 03 / 120), 1.20f, 400, "rico pimiento", 0.95f,
+				Categoria.VEGETAL);
+		Articulo a1 = new Articulo("coliflor", new Date(04 / 03 / 120), 1.20f, 400, "rica coliflor", 1.05f,
+				Categoria.FRUTA);
+		Articulo a2 = new Articulo("pan", new Date(04 / 03 / 120), 1.20f, 400, "rica pan", 1.05f, Categoria.FRUTA);
+		Articulo a3 = new Articulo("agua", new Date(04 / 03 / 120), 1.20f, 400, "rica agua", 1.05f, Categoria.BEBIDA);
+		Articulo a4 = new Articulo("Mandarina", new Date(04 / 03 / 120), 1.20f, 400, "rica mandarina", 1.05f,
+				Categoria.CARNICERIA);
+		ArrayList<Cesta> listaCestas = new ArrayList<Cesta>();
+		Cliente c1 = new Cliente("jokin", "jokin@gmail.com", "hola", "Deusto kalea 1");
+		Cliente c2 = new Cliente("aitor", "aitor@gmail.com", "hola", "Deusto kalea 1");
+		Integer cantidad1 = 400;
+		Integer cantidad2 = 500;
+		Integer cantidad3 = 300;
+		Integer cantidad4 = 600;
+		Cliente mikel = new Cliente("mikel", "mikel@gmail.com", "1234", "Barakaldo");
+		Cliente jokin = new Cliente("jokin", "jokin@gmail.com", "1234", "Universidad Deusto");
+		Cliente ibai = new Cliente("ibai", "ibail@gmail.com", "1234", "Bilbao");
+		Cliente izai = new Cliente("izaia", "izai@gmail.com", "1234", "Universidad");
+		Cliente unai = new Cliente("Unai", "unai@gmail.com", "1234", "DeustoTech");
+		Opinion k = new Opinion("Me ha encantado la lechuga", 7, mikel);
+		Opinion l = new Opinion("No me ha gusatdo el pimiento", 1, jokin);
+		Opinion m = new Opinion("No estaba buena del todo la calabaza pero estaba fresca", 6, ibai);
+		Opinion n = new Opinion("No estaban fresca del todo las fresas pero estaban buenas", 5, izai);
+		Opinion o = new Opinion("Habeis hecho un excelente trabajo", 10, unai);
+
+		ArrayList<Articulo> listaArticulos = new ArrayList<Articulo>();
+		listaArticulos.add(a1);
+		listaArticulos.add(a2);
+		listaArticulos.add(a3);
+		listaArticulos.add(a4);
+		listaArticulos.add(lechuga);
+		listaArticulos.add(pimiento);
+		listaArticulos.add(fresa);
+		listaArticulos.add(calabaza);
+		listaArticulos.add(manzana);
+		store(a1);
+		store(a2);
+		store(a3);
+		store(a4);
+		store(lechuga);
+		store(pimiento);
+		store(fresa);
+		store(calabaza);
+		store(manzana);
+
+		ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
+		listaClientes.add(a);
+		listaClientes.add(b);
+		listaClientes.add(c);
+		listaClientes.add(d);
+		listaClientes.add(e);
+		listaClientes.add(mikel);
+		listaClientes.add(jokin);
+		listaClientes.add(ibai);
+		listaClientes.add(izai);
+		listaClientes.add(unai);
+		store(a);
+		store(b);
+		store(c);
+		store(d);
+		store(e);
+		store(mikel);
+		store(jokin);
+		store(ibai);
+		store(izai);
+		store(unai);
+
+		List<Articulo> listaArticulos1 = new ArrayList<Articulo>();
+		List<Articulo> listaArticulos2 = new ArrayList<Articulo>();
+		List<Integer> listaCantidades1 = new ArrayList<Integer>();
+		List<Integer> listaCantidades2 = new ArrayList<Integer>();
+		listaArticulos1.add(a1);
+		listaArticulos1.add(a2);
+		listaArticulos2.add(a3);
+		listaArticulos2.add(a4);
+		listaCantidades1.add(cantidad1);
+		listaCantidades1.add(cantidad2);
+		listaCantidades2.add(cantidad3);
+		listaCantidades2.add(cantidad4);
+		Cesta aa = new Cesta(1, c1, listaArticulos1, listaCantidades1, Estado.ACTUAL);
+		Cesta bb = new Cesta(2, c2, listaArticulos2, listaCantidades2, Estado.ACTUAL);
+		store(aa);
+		store(bb);
+
+		ArrayList<Opinion> listaOpiniones = new ArrayList<Opinion>();
+		ArrayList<Vendedor> listaVendedores = new ArrayList<Vendedor>();
+		listaVendedores.add(f);
+		listaVendedores.add(g);
+		listaVendedores.add(h);
+		listaVendedores.add(i);
+		listaVendedores.add(j);
+		store(f);
+		store(g);
+		store(h);
+		store(i);
+		store(j);
+
+		listaOpiniones.add(k);
+		listaOpiniones.add(l);
+		listaOpiniones.add(m);
+		listaOpiniones.add(n);
+		listaOpiniones.add(o);
+		store(k);
+		store(l);
+		store(m);
+		store(n);
+		store(o);
+
+	}
+
+	// No cerrar la conexion hasta cerrar el programa
 	public void closeConection() {
 		pmf.close();
 	}
-	
+
 }
