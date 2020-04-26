ede78670c58aa74106dcfb46a951dbb146dabcd7
mikelmartinez999 <mikelmartinez999@opendeusto.es>
2020-04-26 14:33:05 +0200

    Usuario test todo bien menos equals 50%

--- myadmin/src/main/java/es/deusto/spq/data/Usuario.java
+++ myadmin/src/main/java/es/deusto/spq/data/Usuario.java
@@ -44,9 +44,42 @@ public class Usuario implements Serializable {
 
 	@Override
 	public String toString() {
-		return ", nombre=" + nombre + ", contrasenya=" + contrasenya + "]";
+	return ", nombre=" + nombre + ", contrasenya=" + contrasenya + "]";
+	}
+	
+	 @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contrasenya == null) ? 0 : contrasenya.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (contrasenya == null) {
+			if (other.contrasenya != null)
+				return false;
+		} else if (!contrasenya.equals(other.contrasenya))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	
 	
 }
--- myadmin/src/test/java/es/deusto/spq/data/UsuarioTest.java
+++ myadmin/src/test/java/es/deusto/spq/data/UsuarioTest.java
@@ -1,7 +1,9 @@
 package src.test.java.es.deusto.spq.data;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,32 +13,85 @@ import src.main.java.es.deusto.spq.data.*;
 public class UsuarioTest {
 	private Usuario u1;
 	private Usuario u2;
+	private Usuario u3;
 	
 	@Before
 	public void setUp() {
 		u1 = new Usuario("admin", "admin");
 		u2 = new Usuario("jokin", "jokin");
-		u1.setNombre("ander");
-		u1.setContrasenya("ander");
+		u3 = new Usuario();
+		u1.setNombre("mikel");
+		u1.setContrasenya("mikel");
 	}
 	@Test
 	public void testGetId() {
 		assertNotEquals(u1.getId(), u2.getId());
+		assertNotEquals(null, u3.getId());
 	}
 	@Test
 	public void testGetNombre() {
 		assertNotEquals(u1.getNombre(), u2.getNombre());
+		assertNotEquals(u1.getNombre(), u3.getNombre());
 	}
 	@Test
 	public void testSetNombre() {
-		assertEquals(u1.getNombre(), "ander");
+		assertEquals(u1.getNombre(), "mikel");
 	}
 	@Test
 	public void testGetContrasenya() {
 		assertNotEquals(u1.getContrasenya(),u2.getContrasenya());
+		assertEquals(null,u3.getContrasenya());
 	}
 	@Test
 	public void testSetContrasenya() {
-		assertEquals(u1.getContrasenya(), "ander");
+		assertEquals(u1.getContrasenya(), "mikel");
+	}
+	
+	
+	@Test
+	public void testToString() {
+		assertEquals(u1.toString(), 
+				", nombre=" + u1.getNombre() + ", contrasenya=" + u1.getContrasenya() + "]");
+	}
+
+	@Test
+	public void testHashCode() {
+		assertNotEquals(u1.hashCode(),u2.hashCode());
+		assertNotEquals(u2.hashCode(),u1.hashCode());
+		assertNotEquals(u2.hashCode(),u3.hashCode());
+		assertNotEquals(u1.hashCode(),u3.hashCode());
+		assertNotEquals(u3.hashCode(),u2.hashCode());
+		assertNotEquals(u3.hashCode(),u1.hashCode());
+	}
+	
+	@Test
+	public void testEquals() {
+		assertFalse(u1.equals(u2));
+		assertFalse(u1.equals(u3));
+		assertFalse(u2.equals(u1));
+		assertFalse(u2.equals(u3));
+		assertFalse(u3.equals(u1));
+		assertFalse(u3.equals(u2));
+		assertTrue(u1.equals(u1));
+		assertTrue(u2.equals(u2));
+		assertTrue(u3.equals(u3));
+		assertFalse(u1.getNombre().equals(u2.getNombre()));
+		assertFalse(u1.getNombre().equals(u3.getNombre()));
+		assertFalse(u2.getNombre().equals(u1.getNombre()));
+		assertFalse(u2.getNombre().equals(u3.getNombre()));
+		assertTrue(u3.getNombre()==null);
+		assertFalse(u1.getContrasenya().equals(u2.getContrasenya()));
+		assertFalse(u1.getContrasenya().equals(u3.getContrasenya()));
+		assertFalse(u2.getContrasenya().equals(u1.getContrasenya()));
+		assertFalse(u2.getContrasenya().equals(u3.getContrasenya()));
+		assertTrue(u3.getContrasenya()==null);
+		assertFalse(u1.getId() == u2.getId());
+		assertFalse(u1.getId() == u3.getId());
+		assertFalse(u2.getId() == u1.getId());
+		assertFalse(u2.getId()== u3.getId());
+//		assertTrue(u3.getId()==null);
+		
+		
+
 	}
 }
