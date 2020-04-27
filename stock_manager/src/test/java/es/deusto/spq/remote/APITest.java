1e72e33ba40d91d1d58a91ff0d1810bbab5a7db3
jokinme99 <43268907+jokinme99@users.noreply.github.com>
2020-04-28 01:55:26 +0200

    probando

--- stock_manager/src/test/java/es/deusto/spq/remote/APITest.java
+++ stock_manager/src/test/java/es/deusto/spq/remote/APITest.java
@@ -1,30 +1,55 @@
-package es.deusto.spq.remote;
-
-import javax.validation.constraints.Email;
-
-import com.google.gson.JsonObject;
-
-import org.junit.Before;
-import org.junit.Test;
-
-public class APITest {
-
-    JsonObject cliente;
-
-    @Before
-	public void setUp(){
-       cliente = new JsonObject();
-        cliente.addProperty("email", "test@testing.es");
-        cliente.addProperty("password", "123");
-        cliente.addProperty("name", "test-man");
-        cliente.addProperty("address", "TestHouse 22");
-    }
-    @Test
-    public void testRegister() {
-        
-    }
-    @Test
-    public void testLogIn() {
-        
-    }
-}
+//package es.deusto.spq.remote;
+//
+//import static org.junit.Assert.assertTrue;
+//import es.deusto.spq.data.*;
+//import javax.validation.constraints.Email;
+//
+//import javax.json.JsonObject;
+//import javax.json.JsonArray;
+//import javax.json.bind.annotation.JsonbTransient;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.QueryParam;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Request;
+//import javax.ws.rs.core.Response;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.core.MediaType;
+//
+//import com.google.gson.Gson;
+//import com.fasterxml.jackson.annotation.JsonFilter;
+//import com.fasterxml.jackson.core.JsonParser;
+//
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class APITest {
+//
+//    Cliente cliente;
+//    API api;
+//    Gson gson;
+//
+//    @Before
+//	public void setUp(){
+//    gson = new Gson();
+//    cliente = new Cliente("jon", "jon@gmailcom", "jon", "jon kalea");
+//    api = new API();
+//    String obj = cliente.toString();
+//    JsonObject json = gson.fromJson(obj);
+//    api.register(json);
+//    }
+//    
+//    @Test
+//    public void testRegister() {
+//       assertTrue(api.getUsuarios().contains(json));
+//    }
+//    @Test
+//    public void testLogIn() {
+//        
+//    }
+//}
