d79f05a7cc51126774861b647cdb752600cbf965
mikelmartinez999 <mikelmartinez999@opendeusto.es>
2020-05-18 10:11:36 +0200

    Documentacion DAO, Usuario stock, Usuario myadmin y Opinion

--- myadmin/src/main/java/es/deusto/spq/data/Usuario.java
+++ myadmin/src/main/java/es/deusto/spq/data/Usuario.java
@@ -13,57 +13,63 @@ public class Usuario implements Serializable {
 	private String nombre;
 	private String contrasenya;
 	
+	/**Metodo para crear un objeto Usuario vacio
+	 * 
+	 */
 	public Usuario() {
 
 	}
 	
+	/**Contructor de un Usaurio
+	 * @param usuario	nombre con el que vamos a crear el usuario
+	 * @param contrasenya	contrasenya con la que se va a crear al usuario
+	 */
 	public Usuario(String usuario, String contrasenya) {
 		this.id=contador++;
 		this.nombre = usuario;
 		this.contrasenya = contrasenya;
 	}
 	
-//	/**
-//	 * Devuelve el nombre introducido
-//	 * @return Nombre
-//	 */
+
+	/**Metodo para obtener el nombre de un objeto Usuario
+	 * @return	devuelve un objeto String con el nombre del Usuario
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-//	/**
-//	 * Permite modificar el nombre
-//	 * @param nombre
-//	 * @return nombre modificado
-//	 */
+
+	/**Metodo para cambiar el nombre de un Usuario
+	 * @param usuario	nombre a cambiar del Usuario
+	 */
 	public void setNombre(String usuario) {
 		this.nombre = usuario;
 	}
-//	/**
-//	 * Devuelve la contrasenya introducida
-//	 * @return Contrasenya
-//	 */
+
+	
+	/**Metodo para obtener la contrasenya de un Usuario
+	 * @return	devuelve un objeto String con la contrasneya del Usuario
+	 */
 	public String getContrasenya() {
 		return contrasenya;
 	}
-//	/**
-//	 * Permite modificar la contrasenya
-//	 * @param contrasenya
-//	 * @return contrasenya modificada
-//	 */
+
+	/**Metodo para cambiar la contrasenya de un Usuario
+	 * @param contrasenya contrasenya a cambiar del Usuario
+	 */
 	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
-//	/**
-//	 * Devuelve el id introducido
-//	 * @return Id
-//	 */
+
+	/**Metodo para cambiar el id de un Usuario
+	 * @param id	id a cambiar del Usuario
+	 */
 	public long getId() {
 		return id;
 	}
-//	/**
-//	 * Metodo que convierte todos los atributos introducidos a un string.
-//	 * @return String de los atributos de un usuario
-//	 */
+
+	/**Metodo para convertir el objeto Usuario a un String
+	 * @return	devuelve un objeto String con la informacion del Usuario que queremos convertir
+	 */
 	@Override
 	public String toString() {
 	return ", nombre=" + nombre + ", contrasenya=" + contrasenya + "]";
