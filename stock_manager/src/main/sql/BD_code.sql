a08ad1f8e466ad4255f46f422ed798f7dae6414e
Ibaii99 <ibai.guillen@opendeusto.es>
2020-05-19 12:59:44 +0200

    Create chema added

--- stock_manager/src/main/sql/BD_code.sql
+++ stock_manager/src/main/sql/BD_code.sql
@@ -1,3 +1,6 @@
+
+CREATE DATABASE `stock_managerdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 CREATE TABLE `articulo` (
   `ID` bigint(20) NOT NULL,
   `CADUCIDAD` timestamp NULL DEFAULT NULL,
